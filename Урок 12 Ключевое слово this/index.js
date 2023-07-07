7cf94849c937cfcea57c42b2932aadf16184e724
Alextxnk <alexsolov766@gmail.com>
2023-07-07 17:09:13 +0300

    add new files

---
+++ Урок 12 Ключевое слово this/index.js
@@ -0,0 +1,33 @@
+// Ключевое слово this
+
+// Что такое this и зачем он нужен 
+// this - это ключевое слово, которое указывает на текущий контекст выполнения кода
+// чаще всего this являлется именно объектом
+// this - тип данных object
+
+console.log(this); // ссылается глобальный объект window
+
+const user = {
+   name: 'Alex',
+   yearOfBirth: 2002,
+   // 1 способ записи функций в объект
+   getName() {
+      return this.name; // this ссылается на объект user, тк находится внутри этого объекта
+   },
+   calculateAge() {
+      const currentYear = new Date().getFullYear();
+      console.log('currentYear', currentYear);
+      return currentYear - this.yearOfBirth;
+   },
+   // 2 способ записи функций в объект
+   getAllInfo: function() {
+      const age = this.calculateAge();
+      console.log(`Имя: ${this.name}, Возраст: ${age}`);
+   }
+}
+
+console.log(user.getName()); // Alex
+console.log(user.calculateAge()); // 21
+user.getAllInfo(); // Имя: Alex, Возраст: 21
+
+
